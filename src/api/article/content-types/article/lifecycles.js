eea30e6459a970d5f3142be3f64ec08ad001553e
Harald Hartmann <harald@hartmann-dev.net>
2022-09-08 13:29:12 +0200

    umbau

---
+++ src/api/article/content-types/article/lifecycles.js
@@ -0,0 +1,59 @@
+"use strict";
+const utils = require("@strapi/utils");
+
+const slugify = require("slugify");
+
+module.exports = {
+  beforeCreate: async (event) => {
+    const { data } = event.params;
+
+    const locale = data.locale ? data.locale : "de";
+    var rand = Math.floor(1000 + Math.random() * 9000);
+
+    if (data.title && data.type) {
+      event.params.data.slug = slugify(
+        `${locale} ${data.type} ${data.title} ${rand} `,
+        {
+          lower: true,
+          locals: "de",
+        }
+      );
+    }
+  },
+  beforeUpdate: async (event) => {
+    const { data } = event.params;
+
+    const locale = data.slug ? data.slug.split("-")[0] : null;
+    var rand = Math.floor(1000 + Math.random() * 9000);
+
+    if (locale && data.title && data.type) {
+      event.params.data.slug = slugify(
+        `${locale} ${data.type} ${data.title} ${rand}`,
+        {
+          lower: true,
+          locals: "de",
+        }
+      );
+    }
+  },
+  afterCreate: async (event) => {
+    const { result } = event;
+    const { isDraft } = utils.contentTypes;
+    const isDraftState = isDraft(
+      result,
+      strapi.getModel("api::article.article")
+    );
+    if (result.pushNotification == true && !isDraftState)
+      strapi.service("api::article.article").notify(result);
+  },
+  afterUpdate: async (event) => {
+    const { result } = event;
+    const { isDraft } = utils.contentTypes;
+    const isDraftState = isDraft(
+      result,
+      strapi.getModel("api::article.article")
+    );
+    if (result.pushNotification == true && !isDraftState)
+      strapi.service("api::article.article").notify(result);
+  },
+};
--- src/api/article/services/article.js
+++ src/api/article/services/article.js
@@ -1,9 +1,99 @@
-'use strict';
+"use strict";
 
 /**
  * article service.
  */
 
-const { createCoreService } = require('@strapi/strapi').factories;
+const { createCoreService } = require("@strapi/strapi").factories;
+const { Expo } = require("expo-server-sdk");
+let expo = new Expo();
 
-module.exports = createCoreService('api::article.article');
+module.exports = createCoreService("api::article.article", ({ strapi }) => ({
+  async notify(ctx) {
+    const pushTokens = await strapi.db
+      .query("api::push-token.push-token")
+      .findMany({
+        select: ["token"],
+        where: { lang: ctx.locale },
+      });
+
+    let messages = [];
+    for (let pushToken of pushTokens) {
+      // Check that all your push tokens appear to be valid Expo push tokens
+      if (!Expo.isExpoPushToken(pushToken.token)) {
+        console.error(
+          `Push token ${pushToken.token} is not a valid Expo push token`
+        );
+        continue;
+      }
+
+      // Construct a message (see https://docs.expo.io/push-notifications/sending-notifications/)
+      messages.push({
+        to: pushToken.token,
+        sound: "default",
+        body: "Neuer Beitrag in der NoArts! App:  " + data.title,
+        data: { slug: data.slug, title: data.title },
+      });
+    }
+
+    let chunks = expo.chunkPushNotifications(messages);
+    let tickets = [];
+    (async () => {
+      for (let chunk of chunks) {
+        try {
+          let ticketChunk = await expo.sendPushNotificationsAsync(chunk);
+          console.log(ticketChunk);
+          tickets.push(...ticketChunk);
+          // NOTE: If a ticket contains an error code in ticket.details.error, you
+          // must handle it appropriately. The error codes are listed in the Expo
+          // documentation:
+          // https://docs.expo.io/push-notifications/sending-notifications/#individual-errors
+        } catch (error) {
+          console.error(error);
+        }
+      }
+    })();
+
+    let receiptIds = [];
+    for (let ticket of tickets) {
+      // NOTE: Not all tickets have IDs; for example, tickets for notifications
+      // that could not be enqueued will have error information and no receipt ID.
+      if (ticket.id) {
+        receiptIds.push(ticket.id);
+      }
+    }
+
+    let receiptIdChunks = expo.chunkPushNotificationReceiptIds(receiptIds);
+    (async () => {
+      // Like sending notifications, there are different strategies you could use
+      // to retrieve batches of receipts from the Expo service.
+      for (let chunk of receiptIdChunks) {
+        try {
+          let receipts = await expo.getPushNotificationReceiptsAsync(chunk);
+          console.log(receipts);
+
+          // The receipts specify whether Apple or Google successfully received the
+          // notification and information about an error, if one occurred.
+          for (let receiptId in receipts) {
+            let { status, message, details } = receipts[receiptId];
+            if (status === "ok") {
+              continue;
+            } else if (status === "error") {
+              console.error(
+                `There was an error sending a notification: ${message}`
+              );
+              if (details && details.error) {
+                // The error codes are listed in the Expo documentation:
+                // https://docs.expo.io/push-notifications/sending-notifications/#individual-errors
+                // You must handle the errors appropriately.
+                console.error(`The error code is ${details.error}`);
+              }
+            }
+          }
+        } catch (error) {
+          console.error(error);
+        }
+      }
+    })();
+  },
+}));
